bf07406c5ca0a7bf3b8e5604ce6a69015a197950
Priyanka singh <48447484+singhpriyanka549@users.noreply.github.com>
2021-09-10 23:39:36 +0530

    Update todo.service.ts

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 
+export const SERVER_URL: string = "https://jsonplaceholder.typicode.com/todos";
 
-export const SERVER_URL: string = "/api";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  public getTodo() {
+    return this.http.get(SERVER_URL, httpOptions);
+  }
 }
